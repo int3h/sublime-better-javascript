fd3139227bea6e24549b562d9ef817f04607e6f7
Matt Torok <m.torok@sisa.samsung.com>
2013-08-07 16:41:50 -0700

    Turn this into stand-alone plugin
    Now, instead of being some tweaks to the standard JavaScript package, this is a stand-alone version of the tweaked JavaScript package. On install, it will disable the standard JavaScript package and set up ST to use this one instead.

---
+++ Install.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# from __future__ import print_function
+import os.path as path
+import sublime
+
+userSettings = None
+packageName = "Better JavaScript"
+
+
+def getPackageName():
+	global packageName
+	packageDir = path.dirname(path.abspath( __file__ ))
+	if packageDir[0] != "/":
+		return
+	packageName = path.split(packageDir)[1]
+# This needs to be run as soon as possible, because if the cwd changes after loading, the results
+# may be wrong
+getPackageName()
+
+
+def disable_js_package():
+	disabled = userSettings.get("ignored_packages", [])
+
+	if not "JavaScript" in disabled:
+		# Find any views currently using the built-in JS syntaxes and set them to use ours
+		for window in sublime.windows():
+			for view in window.views():
+				syntax = path.basename(view.settings().get('syntax'))
+				if syntax == "JSON.tmLanguage":
+					view.set_syntax_file(path.join("Packages", packageName, "JSON.tmLanguage"))
+				if syntax == "JavaScript.tmLanguage":
+					view.set_syntax_file(path.join("Packages", packageName, "JavaScript.tmLanguage"))
+
+		disabled.append("JavaScript")
+
+		userSettings.set("ignored_packages", disabled)
+		sublime.save_settings("Preferences.sublime-settings")
+
+
+def plugin_loaded():
+	global userSettings
+	userSettings = sublime.load_settings("Preferences.sublime-settings")
+	disable_js_package()
+
+
+# Since ST < 3 does not provide the plugin_loaded() hook, we need to run manually. However, if we
+# run too soon, then the settings loaded will be empty, which can cause problems like all the user
+# settings getting erased. This will wait until we believe the user settings have loaded before
+# execitomg the disable_js_package method.
+def ensureLoaded():
+	if userSettings.has('ignored_packages'):
+		disable_js_package()
+	else:
+		sublime.set_timeout(ensureLoaded, 1000)
+
+if int(sublime.version()) < 3000:
+	userSettings = sublime.load_settings("Preferences.sublime-settings")
+	ensureLoaded()
